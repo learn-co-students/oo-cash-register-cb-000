20f7d3fc359b359c50ebf054c5863983da84bb91
Chuan-Sheng Tai <johnsont426@gmail.com>
2017-03-02 21:32:27 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
 
   def add_item(title, price, quantity=1)
-    @total+=price*quantity
+    self.total+=price*quantity
     quantity.times do
       @items << title
     end
