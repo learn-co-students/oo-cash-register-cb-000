386aee923a2005923df463ed8c36802132a19b15
Dan M <arconk@gmail.com>
2017-04-14 13:03:47 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+  attr_accessor :total
+  attr_reader  :items, :discount
+  def initialize(discount=0)
+    @total=0
+    @discount=discount
+    @items=[]
 
+  end
+
+  def add_item(title, price, quantity=1)
+    
+    previous_items = items.collect{|item|item}
+    @rollback = [@total,previous_items]
+    @total+=price*quantity
+    i = 0
+    while i<quantity
+      @items << title
+      i+=1
+    end
+  end
+
+  def apply_discount
+    @total -= @total*@discount/100
+    if @discount>0
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end 
+  end
+
+  def void_last_transaction
+    @total = @rollback[0]
+    @items = @rollback[1]
+  end
+
+end
