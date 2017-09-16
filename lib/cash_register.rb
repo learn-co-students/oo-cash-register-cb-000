48b9dc8245c9649f5faba0efca55785d9c0eef5f
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2017-09-16 23:02:31 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,53 @@
+require "pry"
+class CashRegister
 
+  attr_accessor :total, :discount, :title, :price, :last_transaction
+  @@items = []
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @@items.clear
+  end
+
+  def add_item(title, price, optional = 0)
+    #binding.pry
+    #@@items.clear
+    @title = title
+    if optional != 0
+    @total += price*optional
+    @last_transaction = price*optional
+    optional.times do
+      @@items << title
+    end
+    else
+     @total += price
+     @last_transaction = price
+     @@items << title
+   end
+  end
+
+  def apply_discount
+    #binding.pry
+      if @discount == 0
+        "There is no discount to apply."
+      else
+        #binding.pry
+        @discount = @discount.to_f/100
+        @discount = @total*@discount
+        @total = @total - @discount
+        @total = @total.to_i
+        "After the discount, the total comes to $800."
+      end
+    end
+
+
+    def items
+    #  binding.pry
+    @@items
+    end
+
+    def void_last_transaction
+      @total = @total - @last_transaction
+    end
+end
