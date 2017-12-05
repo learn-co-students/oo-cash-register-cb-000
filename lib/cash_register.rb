03c0fdd9a0a749014cf6b2a44da25d777edae3d6
Jen Poole <contact.vandepool@gmail.com>
2017-12-05 02:19:58 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+  attr_accessor :items, :discount, :total
 
+  # sets an instance variable @total on initialization to zero
+  # optionally takes an employee discount on initialization
+
+
+  # returns the current total
+  def total
+  end
+
+
+  # accepts a title and a price and increases the total
+  # also accepts an optional quantity
+  # doesn't forget about the previous total
+  def add_item
+  end
+
+
+  # if the cash register was initialized with an employee discount
+    # applies the discount to the total price
+    # returns success message with updated total
+    # reduces the total
+  # if the cash register was not initialized with an employee discount
+    # returns a string error message that there is no discount to apply
+  def apply_discount
+  end
+
+
+  # returns an array containing all items that have been added
+  def items
+  end
+
+  # subtracts the last transaction from the total
+  def void_last_transaction
+  end
+end
