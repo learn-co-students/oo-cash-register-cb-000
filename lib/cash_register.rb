6c28ba1522579402a65c81dc196bfb2956e9de95
Alex Segers <a.segers.dev@gmail.com>
2017-12-27 05:40:50 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,62 +1,32 @@
+
 class CashRegister
-  attr_reader :items, :prices, :discount, :set_total
+  attr_accessor :items, :total, :discount, :last_transaction
 
   def initialize(discount = 0)
-    @discount_applied = false
     @discount = discount
     @items = []
-    @prices = {}
     @total = 0
-    @set_total = nil
   end
 
-  def add_item(item, cost, quantity = 1)
+  def add_item(item, price, quantity = 1)
+    @total += price * quantity
     quantity.times do
       @items << item
     end
-      @prices[item] = cost
-  end
-
-  def total
-    if @set_total != nil
-      @total = @set_total
-      @set_total = nil
-    else
-      @total = 0
-      @items.each do |item|
-        @total += @prices[item]
-      end
-    end
-    @total = @total - (@total * (@discount / 100.00)) if @discount_applied == true
-    @total
-  end
-
-  def total=(total)
-    @set_total = total
-  end
-
-  def apply_discount
-    @discount_applied = true
-    if @discount <= 0
-      return "There is no discount to apply."
-    else
-      return "After the discount, the total comes to $#{(self.total).to_i}."
-    end
+    @last_transaction = price * quantity
   end
 
   def void_last_transaction
-    @items.pop
+    @total -= @last_transaction
+  end
+
+  def apply_discount
+     if @discount > 0
+       @total = @total * (100 - @discount) / 100
+       return "After the discount, the total comes to $#{@total}."
+     else
+       return "There is no discount to apply."
+     end
   end
 
 end
-
-=begin
-cash_register = CashRegister.new(20)
-cash_register.add_item("Lucky Charms", 4.5)
-cash_register.total
-cash_register.add_item("Ritz Crackers", 5.0)
-cash_register.total
-cash_register.add_item("macbook air", 1000)
-cash_register.apply_discount
-
-=end
