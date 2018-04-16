cdb01190e7e81dd527f59fb16d19de16b3f7965a
Marcus Jun <mjun@calstate.edu>
2018-04-16 02:25:42 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,15 +3,21 @@ class CashRegister
 
   attr_accessor :total,:discount,:last_item
 
-  def initialize(*discount)
+  def initialize(discount=0)
     @total=0
+    @discount=discount
     @@all.clear #To clear out any previously added items
-    if discount != nil
-      @discount=discount.join.to_i
-    else
-      @discount=0
-      #puts "There is no discount to apply."
-    end
+
+    #Overly complicated code, works but too long
+    #if discount != nil
+      #I don't know why but discount the argument passed to
+      #initialize, is an array [20] so have to join it into
+      #a string and then convert the string to an integer
+      #Seems weird to do so many conversions.
+      #@discount=discount.join.to_i
+    #else
+      #@discount=0
+    #end
   end
 
   def add_item(item,price,quantity=1)
