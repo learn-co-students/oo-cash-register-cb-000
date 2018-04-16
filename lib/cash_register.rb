75a8677753c1ce262670da4347e5b0e6ac09daad
Marcus Jun <mjun@calstate.edu>
2018-04-16 02:17:21 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,55 @@
+class CashRegister
+  @@all=[]
 
+  attr_accessor :total,:discount,:last_item
+
+  def initialize(*discount)
+    @total=0
+    @@all.clear #To clear out any previously added items
+    if discount != nil
+      @discount=discount.join.to_i
+    else
+      @discount=0
+      #puts "There is no discount to apply."
+    end
+  end
+
+  def add_item(item,price,quantity=1)
+    self.total+=price*quantity
+    self.last_item=price*quantity
+
+    i=0
+    while i<quantity
+      @@all<<item
+      i+=1
+    end
+  end
+
+  def apply_discount
+    if @discount != 0
+      self.total*=((100-@discount)/100.0)#.to_f
+      #self.total.to_i
+      "After the discount, the total comes to $#{@total.to_i}."
+    elsif @discount==0
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @@all
+  end
+
+  def void_last_transaction
+    self.total-=self.last_item
+  end
+
+
+
+
+
+
+
+
+
+
+end
