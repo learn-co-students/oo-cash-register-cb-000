d8d5f841fd4cc1425b1eec3bfcadd10a1c1e51fc
Will Hodgson <will.hodgson@current-rms.com>
2019-08-05 12:51:31 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+require "pry"
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :items_with_prices
+
+  def initialize(employee_discount=0)
+    @total = 0
+    @discount = employee_discount
+    @items = []
+    @items_with_prices = []
+  end
+
+  def add_item(title, price, quantity=1)
+    quantity.times do
+      @items << title
+    end
+    charge_total = quantity * price
+    @total += charge_total
+    @items_with_prices << {title: title, price: price}
+  end
+
+  def apply_discount
+    if @discount > 0
+      @total = @total.to_f * ((100 - @discount.to_f ) / 100)
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    last_item = @items_with_prices.last
+    @total = @total - last_item[:price]
+    @items_with_prices.pop
+    if @items_with_prices.empty?
+      @total = 0
+    end
+  end
+
+end
